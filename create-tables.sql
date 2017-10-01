504238adcbc829d92b231667abcd16d300c17571
Rúben Belém <rubenbelem@gmail.com>
2017-10-01 18:30:17 -0400

    Conserto de erro de git

--- create-tables.sql
+++ create-tables.sql
@@ -1,34 +1,32 @@
-create schema tp2bd_ruben_timoteo;
-
-create table tp2bd_ruben_timoteo.category (
+create table category (
 	id integer primary key,
 	name varchar(100),
 	father_id integer references category (id)
 );
 
-create table tp2bd_ruben_timoteo.productgroup (
+create table productgroup (
 	id serial primary key,
 	name varchar(100) unique
 );
 
-create table tp2bd_ruben_timoteo.product (
+create table product (
 	id integer primary key,
 	asin varchar(11) unique,
 	title varchar(500),
 	salesrank integer
 );
 
-create table tp2bd_ruben_timoteo.product_group (
+create table product_group (
 	product_id integer references product (id),
 	group_id integer references productgroup (id),
 	primary key (product_id, group_id)
 );
 
-create table tp2bd_ruben_timoteo.customer (
+create table customer (
 	amazon_id varchar(25) primary key
 );
 
-create table tp2bd_ruben_timoteo.review ( 
+create table review ( 
 	id serial primary key,
 	date date,
 	rating integer,
@@ -36,27 +34,25 @@ create table tp2bd_ruben_timoteo.review (
 	helpful numeric
 );
 
-create table tp2bd_ruben_timoteo.customer_review (
+create table customer_review (
 	customer_id varchar(25) references customer (amazon_id),
 	review_id integer references review (id)
 );
 
-create table tp2bd_ruben_timoteo.category_product (
+create table category_product (
 	category_id integer references category (id),
 	product_id integer references product (id),
 	primary key (category_id, product_id)
 );
 
-create table tp2bd_ruben_timoteo.similars (
+create table similars (
 	product_id integer references product (id),
 	asin_of_similar varchar(11) references product (asin),
 	primary key (product_id, asin_of_similar)
 );
 
-create table tp2bd_ruben_timoteo.product_review (
+create table product_review (
 	product_id integer references product (id),
 	review_id integer references review (id),
 	primary key (product_id, review_id)
 );
-
-
