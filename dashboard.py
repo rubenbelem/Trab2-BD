7acece352a4095e9f9c21c78bc3fe9b1e941fd1a
Timóteo <mechamutoh@gmail.com>
2017-10-03 22:59:34 -0400

    Criado o dashboard

---
+++ dashboard.py
@@ -0,0 +1,14 @@
+message  = ""
+message += "Seja bem vindo, escolha um comando baseado na letra:\n\n"
+message += "(a) Dado produto, listar os 5 comentários mais úteis e com maior avaliação e os 5 comentários mais úteis e com menor avaliação;\n"
+message += "(b) Dado produto, listar os produtos similares com mais vendas do que ele;\n"
+message += "(c) Dado produto, mostrar a evolução diária das médias de avaliação;\n"
+message += "(d) Listar os 10 produtos líderes de venda em cada grupo de produtos;\n"
+message += "(e) Listar os 10 produtos com a maior média de avaliações úteis positivas por produto;\n"
+message += "(f) Listar as 5 categorias de produto com a maior média de avaliações úteis positivas por produto;\n"
+message += "(g) Listar os 10 clientes que mais fizeram comentários por grupo de produto;\n"
+message += "---"
+
+print(message)
+command = input()
+print(command)
